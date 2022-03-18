c0c9226a45e907bbe7cbdfa760a10ccf43427a75
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-03-18 17:38:44 +0530

    login form add

--- public/Home.js
+++ public/Home.js
@@ -6,3 +6,4 @@ const navbar = document.getElementById('navbar')
 menu.addEventListener('click',()=>{
   navbar.classList.toggle('navtoggle')
 })
+
---
+++ public/signup.js
@@ -0,0 +1,11 @@
+const eye = document.getElementById('eye')
+const eye1 = document.getElementById('eye1')
+
+
+eye1.addEventListener('click',()=>{
+    eye1.classList.toggle('fa-eye-slash')
+  })
+
+eye.addEventListener('click',()=>{
+    eye.classList.toggle('fa-eye-slash')
+  })
