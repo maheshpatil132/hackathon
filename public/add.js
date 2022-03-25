a0515f865e02e7cf2d8bbd03b03773b1c8984026
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-03-25 18:19:19 +0530

    final

--- public/add.js
+++ public/add.js
@@ -1,18 +1,31 @@
 const menu = document.getElementById('menu')
 const navbar = document.getElementById('navbar')
-const open = document.getElementById('open')
+const open_btn = document.getElementById('open')
 const video = document.getElementById('videoElement')
-
-menu.addEventListener('click',()=>{
+let signal = false;
+var data;
+menu.addEventListener('click', () => {
   navbar.classList.toggle('navtoggle')
 })
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function (stream) {
-        video.srcObject = stream;
-      })
-      .catch(function (err0r) {
-        console.log("Something went wrong!");
-      });
+open_btn.addEventListener('click', () => {
+  signal = !signal
+  console.log(signal);
+  if (signal === true) {
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+          console.log("Something went wrong!");
+        });
+    }
   }
+
+
+
+
+})
+
